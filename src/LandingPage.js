dd87bf71db6c60291585375b03a61153625dcae3
Anass Couqua <a.couqua@gmail.com>
2026-06-02 01:08:37 +0100

    feat: 420 SEO landing pages

---
+++ src/LandingPage.js
@@ -0,0 +1,1 @@
+index.js done]cat
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LandingPage from './LandingPage';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Router() {
+  const path = window.location.pathname;
+  const match = path.match(/^\/artisan\/([^/]+)\/([^/]+)\/?$/);
+  if (match) {
+    return <LandingPage serviceSlug={match[1]} citySlug={match[2]} />;
+  }
+  return <App />;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><Router /></React.StrictMode>);
 reportWebVitals();
