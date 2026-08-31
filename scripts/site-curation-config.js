0fda5e3b397359f67ca517d79d5aacb2dc7b8d1f
anassCouqua <71640789+anassCouqua@users.noreply.github.com>
2026-08-31 03:04:10 +0100

    refactor(seo): centralize curated indexable route inventory

---
+++ scripts/site-curation-config.js
@@ -0,0 +1,31 @@
+const INDEXABLE_BLOG_SLUGS = [
+  'trouver-bon-plombier-maroc',
+  'tarif-electricien-maroc-2026',
+  'renovation-maison-maroc-guide',
+  'climatisation-maroc-installation',
+  'serrurier-urgence-maroc',
+  'choisir-carreleur-maroc',
+  'macon-construction-maroc',
+  'urgence-plomberie-casablanca',
+];
+
+const INDEXABLE_SERVICE_CITY_ROUTES = [
+  '/artisan/plombier/casablanca',
+  '/artisan/plombier/rabat',
+  '/artisan/electricien/casablanca',
+  '/artisan/electricien/rabat',
+  '/artisan/macon/casablanca',
+  '/artisan/peintre/casablanca',
+  '/artisan/menuisier/casablanca',
+  '/artisan/climatisation/casablanca',
+  '/artisan/serrurier/casablanca',
+  '/artisan/carreleur/casablanca',
+];
+
+const CORE_ROUTES = ['/', '/about', '/blog', '/contact', '/privacy', '/terms'];
+
+module.exports = {
+  INDEXABLE_BLOG_SLUGS,
+  INDEXABLE_SERVICE_CITY_ROUTES,
+  CORE_ROUTES,
+};
